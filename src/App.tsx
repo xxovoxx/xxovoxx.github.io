0067e8224e4e91ebd1202c6e216e0917f99d245b
Xx_OVO_xX <1003477166@qq.com>
2024-09-10 20:22:39 +0800

    add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2 className='underline'>tailwindcss hello world</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
